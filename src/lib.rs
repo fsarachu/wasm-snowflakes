1e472a0fbbb3b32d4bc7f378e5898348d0635acd
fsarachu <fsarachu@gmail.com>
2019-03-11 21:24:58 -0300

    Snowflakes!

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,77 @@
 extern crate cfg_if;
+extern crate js_sys;
 extern crate wasm_bindgen;
 
 mod utils;
 
-use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
 
-cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
-    if #[cfg(feature = "wee_alloc")] {
-        extern crate wee_alloc;
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+fn rand_between(min: f64, max: f64) -> f64 {
+    (js_sys::Math::random() * (max - min) + min) as f64
+}
+
+#[wasm_bindgen]
+pub struct Snowflake {
+    max_width: f64,
+    max_height: f64,
+    x: f64,
+    y: f64,
+    velocity_x: f64,
+    velocity_y: f64,
+    radius: f64,
+    alpha: f64,
+}
+
+#[wasm_bindgen]
+impl Snowflake {
+    pub fn tick(&mut self) {
+        self.x = self.x + self.velocity_x;
+        self.y = self.y + self.velocity_y;
+
+        if self.y + self.radius > self.max_height {
+            self.x = rand_between(0.0, self.max_width);
+            self.y = rand_between(0.0, -self.max_height);
+            self.velocity_x = rand_between(-3.0, 3.0);
+            self.velocity_y = rand_between(2.0, 5.0);
+            self.radius = rand_between(1.0, 2.0);
+            self.alpha = rand_between(0.1, 0.9);
+        }
     }
-}
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
+    pub fn new(max_width: f64, max_height: f64) -> Snowflake {
+        Snowflake {
+            max_width,
+            max_height,
+            x: rand_between(0.0, max_width),
+            y: rand_between(0.0, max_height),
+            velocity_x: rand_between(-3.0, 3.0),
+            velocity_y: rand_between(1.0, 5.0),
+            radius: rand_between(1.0, 4.0),
+            alpha: rand_between(0.1, 0.9),
+        }
+    }
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+    pub fn x(&self) -> f64 {
+        self.x
+    }
+
+    pub fn y(&self) -> f64 {
+        self.y
+    }
+
+    pub fn velocity_x(&self) -> f64 {
+        self.velocity_x
+    }
+
+    pub fn velocity_y(&self) -> f64 {
+        self.velocity_y
+    }
+
+    pub fn radius(&self) -> f64 {
+        self.radius
+    }
+
+    pub fn alpha(&self) -> f64 {
+        self.alpha
+    }
 }
